fa092b26d1dca6794325129de90dad60f4d8075f
Allan Ortiz <aortez@gmail.com>
2025-05-18 15:06:03 -0700

    initial commit
    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+Playground for yocto, zephr, and lvgl.
+
+Enter the venv like so:
+
+    source .venv/bin/activate
---
+++ test-lvgl/test-lvgl.cpp
@@ -0,0 +1,13 @@
+#include "config.h"
+
+#include "lv_conf.h"
+#include "lvgl/src/drivers/wayland/lv_wayland.h"
+#include "lvgl/lvgl.h"
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+	printf("Hello there\n");
+
+	return 0;
+}
